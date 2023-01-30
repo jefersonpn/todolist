2ceeb20bf283a423039236fd0a482c5a17646083
Jeferson Nascimento <67881060+jefersonpn@users.noreply.github.com>
2023-01-30 20:24:31 +0100

    finish but bug on checkbox

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,20 @@
 import './bootstrap';
+
+import { createApp } from 'vue';
+import App from './vue/App.vue';
+
+/* import the fontawesome core */
+import { library } from '@fortawesome/fontawesome-svg-core'
+
+/* import font awesome icon component */
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+
+/* import specific icons */
+import { faPlusSquare, faTrash } from '@fortawesome/free-solid-svg-icons'
+
+/* add icons to the library */
+library.add(faPlusSquare, faTrash)
+
+createApp(App)
+.component('font-awesome-icon', FontAwesomeIcon)
+.mount("#app")
